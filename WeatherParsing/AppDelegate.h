35ec8b8c49f5902a0d099ba194680c6db31725d4
Harshad <harshad@Harshads-Mac-Pro.local>
2016-08-27 10:37:27 +0530

    Initial Commit

---
+++ WeatherParsing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherParsing
+//
+//  Created by Harshad on 27/08/16.
+//  Copyright (c) 2016 Harshad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeatherParsing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeatherParsing
+//
+//  Created by Harshad on 27/08/16.
+//  Copyright (c) 2016 Harshad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
